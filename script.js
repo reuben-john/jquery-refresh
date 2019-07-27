2877c48fbf368bd1ec63c491b9e8c27eaa3eac0f
Reuben <rjkoppy@gmail.com>
2019-07-27 11:51:47 -0500

    Add info on selectors

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-$(document).ready(function() {});
+$(document).ready(function() {
+  // Selectors
+
+  $("button").click(function() {
+    // any valid css can be used here
+    $("p").hide(); // selects all <p> tags
+    $("#test").hide(); // select element with id test
+    $(".test").hide(); // select element with class test
+    $("*").hide(); // hide all elements
+    $("p.intro").hide(); // select p with intro class
+    $(":button").hide(); // all button elements and type="button"
+    $("tr:even").hide(); // all even tr elements
+    $("ul lu:first").hide(); // first li element of first ul
+    $("ul lu:first-child").hide(); // first li element of every ul
+  });
+});
